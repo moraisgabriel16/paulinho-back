5c2e6a8ef9641e2202a43f9e40b69637deea9a2b
Gabriel Morais <gabriel@example.com>
2026-02-03 14:31:16 -0300

    Add: Simple test endpoint

---
+++ api/test.ts
@@ -0,0 +1,10 @@
+import { VercelRequest, VercelResponse } from '@vercel/node';
+
+export default (req: VercelRequest, res: VercelResponse) => {
+  res.status(200).json({
+    status: 'API online',
+    method: req.method,
+    path: req.url,
+    timestamp: new Date().toISOString(),
+  });
+};
